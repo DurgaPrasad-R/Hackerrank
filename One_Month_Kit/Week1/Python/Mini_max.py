30532ceb98801d6f60827d6157a6d1a52f692a12
Runkana Durga Prasad <111352968+DurgaPrasad-R@users.noreply.github.com>
2023-05-10 23:30:29 +0530

    Update Mini_max.py

--- One_Month_Kit/Week1/Python/Mini_max.py
+++ One_Month_Kit/Week1/Python/Mini_max.py
@@ -7,10 +7,6 @@ import re
 import sys
 
 
-# Complete the 'miniMaxSum' function below.
-#
-# The function accepts INTEGER_ARRAY arr as parameter.
-
 def miniMaxSum(arr):
     # Write your code here
     arr.sort()
