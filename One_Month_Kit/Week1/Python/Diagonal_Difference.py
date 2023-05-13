18646bf1c190d473d674beda5bef59d1bbd51c69
Runkana Durga Prasad <111352968+DurgaPrasad-R@users.noreply.github.com>
2023-05-13 23:32:29 +0530

    Create Diagonal_Difference.py

---
+++ One_Month_Kit/Week1/Python/Diagonal_Difference.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def diagonalDifference(arr):
+    l = 0
+    r = 0
+    for i in range(len(arr)):
+        l+=arr[i][i]
+        r+=arr[i][len(arr)-1-i]
+    return abs(l-r)
+        
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
